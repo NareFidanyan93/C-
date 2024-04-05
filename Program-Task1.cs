5a480cd95baebe395e8b224717468fef4fe20afb
NareFidanyan93 <165837836+NareFidanyan93@users.noreply.github.com>
2024-04-06 02:27:14 +0400

    Add files via upload

---
+++ Program-Task1.cs
@@ -0,0 +1,30 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа в промежутке от M до N.
+//  Использовать рекурсию, не использовать циклы.
+
+void Main()
+{
+    int M = ReadInt("Введите число M : ");
+    int N = ReadInt("Введите число N : ");
+     PrintNumbers(N , M);
+
+}
+
+void PrintNumbers(int N, int M)
+{
+
+    if(N < M || N < 0) return;
+
+    PrintNumbers(N - 1, M );
+    Console.Write(N + " ");
+}
+
+
+
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+Main();
---
+++ Program-Task2.cs
@@ -0,0 +1,34 @@
+﻿// Задача 2: Напишите программу 
+// вычисления функции Аккермана с помощью рекурсии.
+//  Даны два неотрицательных числа m и n.
+
+
+void Main()
+{
+    int m = ReadInt("Введите число m : ");
+    int n = ReadInt("Введите число n : ");
+    Akkerman (m , n);
+    Console.Write(Akkerman(m , n));
+}
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+     if (n == 0 && m > 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else
+    {
+        return (Akkerman(m - 1, Akkerman(m, n - 1)));
+    }
+}
+int ReadInt(string msg)
+{
+    Console.Write(msg);
+    return Convert.ToInt32(Console.ReadLine());
+}
+Main();
